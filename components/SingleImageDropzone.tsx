13ca1eeebc8bcec1342e38f1306001ea9543d028
nerdy1001 <138679014+nerdy1001@users.noreply.github.com>
2024-01-03 12:17:46 +0100

    Update SingleImageDropzone.tsx

--- components/SingleImageDropzone.tsx
+++ components/SingleImageDropzone.tsx
@@ -118,7 +118,7 @@ const SingleImageDropzone = React.forwardRef<HTMLInputElement, InputProps>(
           return ERROR_MESSAGES.fileNotSupported();
         }
       }
-      return undefined;
+      return "undefined";
     }, [fileRejections, dropzoneOptions]);
 
     return (
